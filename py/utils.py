8c290da95a87a1bfd9da46cfd66d137c925ffc46
atsushimemet <noap3b69n@gmail.com>
2024-01-08 17:38:30 +0900

    refactor: vscode warning

--- py/utils.py
+++ py/utils.py
@@ -1,12 +1,16 @@
 import os
 from datetime import datetime
-import logzero
-from oauth2client.service_account import ServiceAccountCredentials
+
 import gspread
-import pandas as pd
+import logzero
 import matplotlib.pyplot as plt
+import pandas as pd
 from logzero import logger
+from oauth2client.service_account import ServiceAccountCredentials
+
 from config import designated_date
+from validates import validates_24hours
+
 
 class ColorCodes:
     SLEEP = "#AD1357"
@@ -20,6 +24,7 @@ class ColorCodes:
     MOVE = "#616161"
     PERSONAL = "#7886CB"
 
+
 class LogSettingRunner:
     def run():
         # ログディレクトリのパス
@@ -32,20 +37,19 @@ class LogSettingRunner:
         # ファイル名を生成
         log_file_name = f"{timestamp}.txt"
 
-        # ログファイルのフルパスを生成
-        log_file_path = os.path.join(log_directory, log_file_name)
-
         # ログの設定
-        logzero.logfile(os.path.join(log_directory, log_file_name))  # ログファイルのパスを指定
+        logzero.logfile(os.path.join(log_directory, log_file_name))
 
 
-def get_worksheet_values(json_path: str, ss_id: str, sheet_name:str):
+def get_worksheet_values(json_path: str, ss_id: str, sheet_name: str):
     scopes = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive",
     ]
     # Credentials 情報を取得
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_path, scopes)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        json_path, scopes
+    )  # noqa
     # OAuth2のクレデンシャルを使用してGoogleAPIにログイン
     gc = gspread.authorize(credentials)
     # IDを指定して、Googleスプレッドシートのワークブックを選択する
@@ -71,6 +75,7 @@ def create_df_long(sheet: list) -> pd.DataFrame:
     move_list = sheet[10][1:]
     study_list = sheet[11][1:]
     sum_list = sheet[12][1:]
+    validates_24hours(sum_list)
 
     tmp = pd.DataFrame(
         {
@@ -89,8 +94,11 @@ def create_df_long(sheet: list) -> pd.DataFrame:
         }
     )
 
-    tmp = tmp.melt(id_vars=["date", "weekday"], var_name="calendar", value_name="time")
+    tmp = tmp.melt(
+        id_vars=["date", "weekday"], var_name="calendar", value_name="time"
+    )  # noqa
     return tmp.sort_values(["date", "weekday", "calendar"])
+
 
 def correct_bad_records(df: pd.DataFrame, bad_rec_index: list) -> pd.DataFrame:
     for idx in bad_rec_index:
@@ -101,19 +109,21 @@ def correct_bad_records(df: pd.DataFrame, bad_rec_index: list) -> pd.DataFrame:
         if t.count("#NUM!") == 0:
             break
         elif t.count("#NUM!") == 1:
-            i = t.index("#NUM!")
-            complement = str(24.00 - sum([float(v) for v in t if v != "#NUM!"]))
+            complement = str(24.00 - sum([float(v) for v in t if v != "#NUM!"]))  # noqa
             df.loc[idx] = [d, w, c, complement]
         else:
             c = t.count("#NUM!")
-            complement = str((24.00 - sum([float(v) for v in t if v != "#NUM!"])) / c)
+            complement = str(
+                (24.00 - sum([float(v) for v in t if v != "#NUM!"])) / c
+            )  # noqa
             df_tmp = df[df["date"] == d].replace("#NUM!", complement)
             df = pd.concat([df[df["date"] != d], df_tmp])
     return df
 
-def output_graph(df:pd.DataFrame, window: int, save:bool=False):
-    plt.rcParams['font.family'] = 'Times New Roman' # font familyの設定
-    
+
+def output_graph(df: pd.DataFrame, window: int, save: bool = False):
+    plt.rcParams["font.family"] = "Times New Roman"  # font familyの設定
+
     color = [
         ColorCodes.SLEEP,
         ColorCodes.REGULARREVENUE,
@@ -128,7 +138,14 @@ def output_graph(df:pd.DataFrame, window: int, save:bool=False):
     ]
 
     # 積み上げ棒グラフを描画
-    ax = df.plot(kind="bar", stacked=True, figsize=(10, 7), color=color, edgecolor="#00081A", alpha=0.75)
+    ax = df.plot(
+        kind="bar",
+        stacked=True,
+        figsize=(10, 7),
+        color=color,
+        edgecolor="#00081A",
+        alpha=0.75,
+    )
 
     # グラフにタイトルと軸ラベルを追加
     plt.title("Stacked Bar Chart of Time by Google Calendar")
@@ -148,7 +165,7 @@ def output_graph(df:pd.DataFrame, window: int, save:bool=False):
     # y軸の範囲を0から24までに設定
     plt.ylim(0, 24)
     plt.yticks(range(0, 25, 3))
-    
+
     # グラフを保存
     if save:
         # 現在の日時を取得
@@ -162,7 +179,7 @@ def output_graph(df:pd.DataFrame, window: int, save:bool=False):
         elif window == 30:
             plt.savefig(f"./graph/monthly/{graph_file_name}")
         else:
-            logger.warning(f"bad window config is set.")
+            logger.warning("bad window config is set.")
 
     # グラフを表示
     plt.show()
--- py/validates.py
+++ py/validates.py
@@ -1,5 +1,7 @@
-import pandas as pd
 from datetime import datetime, timedelta
+
+import pandas as pd
+
 
 def validate_calendar_null_date(tmp: list):
     start_date = tmp[0][1]
@@ -14,14 +16,19 @@ def validate_calendar_null_date(tmp: list):
         "Not all calendar information for the dates is available."
     )
 
+
 def validates_24hours(sum_list: list):
-    assert set(sum_list) == {"24.00"}, AssertionError("Not 24 hours date exists.")
+    assert set(sum_list) == {"24.00"}, AssertionError(
+        "Not 24 hours date exists."
+    )  # noqa
+
 
 def validate_bad_records(df: pd.DataFrame):
     assert not df[df["time"] == "#NUM!"].shape[0], AssertionError(
         "Bad records exist."
-    )
+    )  # noqa
     print("Bad records num:", df[df["time"] == "#NUM!"].shape[0])
+
 
 def validate_data_types(df):
     expected_data_types = {
@@ -34,15 +41,18 @@ def validate_data_types(df):
     for column, expected_type in expected_data_types.items():
         if df[column].dtype != expected_type:
             raise ValueError(
-                f"Column '{column}' should be of type {expected_type}, but it is {df[column].dtype}"
+                f"Column '{column}' should be of type {expected_type}, but it is {df[column].dtype}"  # noqa
             )
 
     print("Data types validation passed!")
 
-def validates_calculate_moving_average(df_long: pd.DataFrame, df_tmp: pd.DataFrame, check_date: str, window:int):
+
+def validates_calculate_moving_average(
+    df_long: pd.DataFrame, df_tmp: pd.DataFrame, check_date: str, window: int
+):
     check_date_p = datetime.strptime(check_date, "%Y-%m-%d")
     # X日前の日付を計算
-    start_date_p = check_date_p - timedelta(days=window-1)
+    start_date_p = check_date_p - timedelta(days=window - 1)
     # 新しい日付をyyyy-mm-dd形式の文字列型に変換
     start_date_p = start_date_p.strftime("%Y-%m-%d")
     df_tmp_20240101_raw = df_long.loc[start_date_p:check_date]
@@ -50,7 +60,9 @@ def validates_calculate_moving_average(df_long: pd.DataFrame, df_tmp: pd.DataFra
         "time"
     ].mean()
     df_tmp_20240101 = df_tmp[df_tmp["date"] == "2024/01/01"]
-    expected = df_tmp_20240101[df_tmp_20240101["calendar"] == "friends"]["time"].values[
+    expected = df_tmp_20240101[df_tmp_20240101["calendar"] == "friends"][
+        "time"
+    ].values[  # noqa
         0
     ]
     assert actual == expected, AssertionError(
